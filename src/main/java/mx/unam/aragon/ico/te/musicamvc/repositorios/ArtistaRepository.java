74bac576b39fadfed4eb8abbd747ad1fcbe4a074
Moises Mares <moises.mares.m@gmail.com>
2025-06-04 21:12:55 -0600

    Commit inicial

---
+++ src/main/java/mx/unam/aragon/ico/te/musicamvc/repositorios/ArtistaRepository.java
@@ -0,0 +1,8 @@
+package mx.unam.aragon.ico.te.musicamvc.repositorios;
+
+import mx.unam.aragon.ico.te.musicamvc.modelos.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistaRepository extends JpaRepository<Artista, Integer> {
+
+}
