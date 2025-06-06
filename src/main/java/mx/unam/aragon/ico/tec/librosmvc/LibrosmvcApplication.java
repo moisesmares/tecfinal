2b896b855409e32e3445e4219a4f04561c934e20
Moises Mares <moises.mares.m@gmail.com>
2025-06-05 19:13:20 -0600

    Primeros 2 templates para HTML

---
+++ src/main/java/mx/unam/aragon/ico/tec/librosmvc/LibrosmvcApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.tec.librosmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  LibrosmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/unam/aragon/ico/tec/librosmvc/LibrosmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.tec.librosmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
