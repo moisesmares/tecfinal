9b2850885d93ed8b8e9e9b323fc42858f4650b1d
Moises Mares <124109300+moisesmares@users.noreply.github.com>
2025-06-05 20:04:20 -0600

    Delete src/main/java/mx/unam/aragon/ico/te/musicamvc directory

--- src/main/java/mx/unam/aragon/ico/te/musicamvc/repositorios/ArtistaRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.unam.aragon.ico.te.musicamvc.repositorios;
-
-import mx.unam.aragon.ico.te.musicamvc.modelos.Artista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistaRepository extends JpaRepository<Artista, Integer> {
-
-}
